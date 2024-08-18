3f88f5b00f93748f0ec1f513030cb5461563963a
Sudip Malla <sudipmallathakuri@gmail.com>
2024-08-18 17:16:45 +0545

    Update Testimonial.jsx

--- src/components/pages_components/Testimonial.jsx
+++ src/components/pages_components/Testimonial.jsx
@@ -4,7 +4,8 @@ import image2 from "../../assets/images/2.jpg";
 import image3 from "../../assets/images/3.jpg";
 import image4 from "../../assets/images/4.jpg";
 
-import OwlCarousel from "react-owl-carousel";
+import OwlCarousel from "react-owl-carousel/dist/OwlCarousel";
+
 import "../../style/testimonial.css";
 export default function Testimonial() {
   const options = {
