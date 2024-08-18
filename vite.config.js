02152dbb328cc60623dd3f4cae78f8dc8bb10421
Sudip Malla <sudipmallathakuri@gmail.com>
2024-08-18 17:15:04 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 import inject from '@rollup/plugin-inject';
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
+build: {
     rollupOptions: {
-      external: ['react-owl-carousel']
+      external: ['react-owl-carousel/dist/OwlCarousel']
     }
   },
   plugins: [
